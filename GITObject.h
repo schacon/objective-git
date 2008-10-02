752ffa52bc1b9f48f1b6e7c3aadeffd8d7fa961c
Brian Chapados <chapados@sciencegeeks.org>
2008-10-01 21:06:49 -0700

    Refactor ObjGitObject into GITObject.
    Cleaned up ivars/accessors and rewrote #parseRaw to remove
    deprecated stringWithCString:length: calls.  Also replaced
    cryptic memstr()-searching for '\0' to use strlen.  The new
    code, uses strlen to read the header, and stores the contents
    in an NSData ivar.

---
+++ GITObject.h
@@ -0,0 +1,35 @@
+//
+//  GITObject.h
+//  ObjectiveGit
+//
+//  Created by chapbr on 10/1/08.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GITObject : NSObject {
+	NSString* sha;
+	NSUInteger size;
+	NSString* type;
+	NSUInteger rawContentLen;
+	NSData*   raw;
+	NSData* contentsData;
+}
+
+@property(copy, readwrite) NSString *sha;	
+@property(assign, readwrite) NSUInteger size;	
+@property(copy, readwrite) NSString *type;	
+@property(assign, readwrite) NSUInteger rawContentLen;
+@property(copy, readwrite) NSData   *raw;	
+@property(copy, readwrite) NSData   *contentsData;	
+
+- (id) initFromRaw:(NSData *)rawData withSha:(NSString *)shaValue;
+- (void) parseRaw;
+
+- (NSString *) contents;
+
+- (void) setRaw:(NSData *) rawData inflate:(BOOL) inflate;
+- (NSData *) inflateRaw:(NSData *)rawData;
+
+@end
---
+++ GITObject.m
@@ -0,0 +1,79 @@
+//
+//  GITObject.m
+//  ObjectiveGit
+//
+//  Created by chapbr on 10/1/08.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import "GITObject.h"
+#import "NSDataCompression.h"
+
+@implementation GITObject
+
+@synthesize sha;
+@synthesize size;
+@synthesize type;
+@synthesize raw;
+@synthesize contentsData;
+
+@synthesize rawContentLen;
+
+- (id) initFromRaw:(NSData *)rawData withSha:(NSString *)shaValue
+{
+	if (! [super init])
+		return nil;
+
+	[self setSha:shaValue];
+	[self setRaw:rawData inflate:YES];
+	[self parseRaw];
+	return self;
+}
+
+- (void) parseRaw
+{
+	char *bytes = (char *)[[self raw] bytes]; 
+    NSUInteger len = [[self raw] length];
+
+	//NSData *headerData = [NSData dataWithBytes:bytes length:strlen(bytes)];
+	NSString *header = [[NSString alloc] initWithBytes:bytes 
+												length:strlen(bytes) 
+											  encoding:NSASCIIStringEncoding];
+
+	NSArray *headerChunks = [header componentsSeparatedByString:@" "];
+	[header release];
+	
+	[self setType:[headerChunks objectAtIndex:0]];
+	[self setSize:[[headerChunks objectAtIndex:1] intValue]];
+	
+	NSUInteger contentOffset = strlen(bytes) + 2;
+	rawContentLen = len - contentOffset;
+	NSRange contentRange = NSMakeRange(contentOffset, rawContentLen);
+	NSData *contentData = [[self raw] subdataWithRange:contentRange];
+	[self setContentsData:contentData];
+}
+
+- (NSUInteger) getRawContents:(char *) buf;
+{
+	return 0;
+}
+
+- (NSString *) contents;
+{
+	return [[[NSString alloc] initWithData:contentsData encoding:NSASCIIStringEncoding] autorelease];
+}
+
+- (void) setRaw:(NSData *) rawData inflate:(BOOL) inflate;
+{
+	if (inflate)
+		[self setRaw:[rawData decompressedData]];
+	else
+		[self setRaw:rawData];
+}
+
+- (NSData *) inflateRaw:(NSData *)rawData
+{
+	return [rawData decompressedData];
+}
+
+@end
